cd68c8e883ac46655442bdb4e7906bfd395a2ac2
Vijay Singh <codearmaan2022@gmail.com>
2024-01-15 10:13:49 +0000

    basci dom manipulation

---
+++ dom-projects/01-colorChanger/script.js
@@ -0,0 +1,29 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body')
+
+buttons.forEach((button) => {
+    // console.log(button);
+    button.addEventListener('click', (e) => {
+        // console.log(e);
+        console.log(e.target);
+        console.log(e.target.id)
+        body.style.backgroundColor = e.target.id
+        switch (e.target.id) {
+            case 'grey':
+                body.style.backgroundColor = e.target.id
+                break;
+            case 'white':
+                body.style.backgroundColor = e.target.id
+                break;
+            case 'blue':
+                body.style.backgroundColor = e.target.id
+                break;
+            case 'yellow':
+                body.style.backgroundColor = e.target.id
+                break;
+            default:
+                body.style.backgroundColor = 'white'
+                break;
+        }
+    })
+});
